0b426e293b7ee8a9f379652bec4117eaf8c437d4
ashirbad <ashirbadbehera29@gmail.com>
2021-05-21 18:17:05 +0530

    🚀 add some features

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -7,7 +7,10 @@ import images from './images.js';
   * function and varialbe name snake_case -> is_prime
   * content varialbe upper case PI
 */
-
+/**
+ * @param {String} secreat_word
+ * @param {Array} letters_guessed
+ */
 const is_word_guessed = (secreat_word, letters_guessed) => {
 	/*
   secret_word: word guess by the user
@@ -16,8 +19,10 @@ const is_word_guessed = (secreat_word, letters_guessed) => {
       return True (if user guess the world correctly )
       return False (wrong selection)
   */
-
-	return false;
+	for (let char of secreat_word) {
+		if (letters_guessed.includes(char) == false) return false;
+	}
+	return true;
 };
 
 // if you want to test this function please call function -> get_guessed_word("kindness", [k, n, d])
@@ -25,7 +30,7 @@ const is_word_guessed = (secreat_word, letters_guessed) => {
  * @param {String} secreat_word
  * @param {Array} letters_guessed
  */
-const get_guessed_word = (secret_word, letters_guessed) => {
+const get_guessed_word = (secreat_word, letters_guessed) => {
 	/*
     secret_word: word to guess by the user
     letters_guessed: list hold all the word guess by the user
@@ -81,38 +86,57 @@ const hangman = secreat_word => {
   */
 
 	console.log('Welcome to the game, Hangman!');
-	console.log(`I am thinking of a word that is ${secreat_word.length} \n`);
+	console.log(
+		`I am thinking of a word that is of length ${secreat_word.length} \n`
+	);
 
 	const letters_guessed = [];
+	let index = 0;
+	while (index < images.length) {
+		const available_letters = get_available_letters(letters_guessed);
+		console.log(`Availalbe letters: ${available_letters}`);
 
-	const available_letters = get_available_letters(letters_guessed);
-	console.log(`Availalbe letters: ${available_letters}`);
-	const guess = read_line_sync.question('Please guess a letter: ');
-	const letter = guess.toLowerCase();
+		const guess = read_line_sync.question('Please guess a letter: ');
+		const letter = guess.toLowerCase();
 
-	if (secreat_word.includes(letter)) {
-		letters_guessed.push(letter);
-		console.log(
-			`Good guess: ${get_guessed_word(secreat_word, letters_guessed)}`
-		);
-
-		if (is_word_guessed(secreat_word, letters_guessed)) {
-			console.log('* * Congratulations, you won! * *');
+		if (letter.length > 1) {
+			console.log('Enter Only a Single Character from available words');
+			continue;
 		}
-	} else {
-		console.log(
-			`Oops! That letter is not in my word: ${get_guessed_word(
-				secret_word,
-				letters_guessed
-			)}`
-		);
-		letters_guessed.push(letter);
-		console.log('');
+
+		if (secreat_word.includes(letter)) {
+			letters_guessed.push(letter);
+			console.log(
+				`Good guess: ${get_guessed_word(secreat_word, letters_guessed)}`
+			);
+
+			if (is_word_guessed(secreat_word, letters_guessed)) {
+				console.log('* * Congratulations, you won! * *');
+				break;
+			}
+		} else {
+			console.log(
+				`Oops! That letter is not in my word: ${get_guessed_word(
+					secreat_word,
+					letters_guessed
+				)}`
+			);
+
+			console.log(images[index++]);
+
+			if (index == images.length) {
+				console.log("You Couldn't guessed it, Better try next time...");
+				console.log(`The Correct Word is ${secreat_word} 😄`);
+			}
+
+			letters_guessed.push(letter);
+			console.log('');
+		}
 	}
 };
 
 const secreat_word = choose_word();
-// hangman(secreat_word);
+hangman(secreat_word);
 
 // * testing stuff
 // console.log(get_guessed_word('kindness', ['k', 'n', 's'])); // working
